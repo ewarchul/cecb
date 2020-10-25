2085b64968c5e655a8dfb4d63a225b2c514d79a1
ewarchul <ewarchul@gmail.com>
2020-10-25 09:44:52 +0100

    Fix unit tests

--- tests/testthat/test-config.R
+++ tests/testthat/test-config.R
@@ -16,21 +16,26 @@ testthat::test_that("function correctly parses yaml file", {
 	testthat::expect_equal(config_fields, 11)
 	testthat::expect_equal(number_of_dims, 2)
 	testthat::expect_equal(cpu_usage, 0.75)
-	testthat::expect_equal(number_of_methods, 2)
+	testthat::expect_equal(number_of_methods, 1)
 })
 
 testthat::test_that("function properly extracts names of algorithms", {
+	# Given
 	parsed_config = 
 		parse_yaml_config(config_path)
+	# When
 	alg_name = 
-		cecb::extract_names(2, parsed_config$methods)
-	testthat::expect_equal(alg_names, c("optim_alg", "optim_alg"))
+		cecb::extract_names(1, parsed_config$methods)
+    # Then
+	testthat::expect_equal(alg_name, "optim_alg")
 })
 
 
 testthat::test_that("function is able to distinguish correct and incorrect yaml configs", {
+	# When
 	parsed_config = 
-		parse_yaml_config(config_path)
+		yaml::read_yaml(config_path)
+	# Then	
 	testthat::expect_true(verify_config_names(parsed_config))
 })
 
